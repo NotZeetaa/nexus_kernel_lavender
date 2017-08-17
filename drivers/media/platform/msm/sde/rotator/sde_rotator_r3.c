48a59c513c69c745460dde8d9f9061bad238bc5b
Sami Tolvanen <samitolvanen@google.com>
2017-08-17 14:07:29 -0700

    msm: sde: fix unaligned memory read
    Prevent the compiler from combining two 32b writes to 4B-aligned iomem
    address, into a single 64b write to a non-8B-aligned iomem address,
    where the iomem address is fixed and cannot be realigned.
    
    Prevents a kernel panic
    ("Unhandled fault: alignment fault (0x96000061)").
    
    Bug: 64527440
    Bug: 62093296
    Change-Id: I6dafc7427b5c52eb390beebabd5281133889bddc
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

--- drivers/media/platform/msm/sde/rotator/sde_rotator_r3.c
+++ drivers/media/platform/msm/sde/rotator/sde_rotator_r3.c
@@ -49,56 +49,37 @@
 /* Macro for constructing the REGDMA command */
 #define SDE_REGDMA_WRITE(p, off, data) \
 	do { \
-		SDEROT_DBG("SDEREG.W:[%s:0x%X] <= 0x%X\n", #off, (off),\
-				(u32)(data));\
-		writel_relaxed_no_log( \
-				(REGDMA_OP_REGWRITE | \
-				 ((off) & REGDMA_ADDR_OFFSET_MASK)), \
-				p); \
-		p += sizeof(u32); \
-		writel_relaxed_no_log(data, p); \
-		p += sizeof(u32); \
+		writel_relaxed(REGDMA_OP_REGWRITE | \
+			((off) & REGDMA_ADDR_OFFSET_MASK), (p)); \
+		(p)++; \
+		writel_relaxed((data), (p)); \
+		(p)++; \
 	} while (0)
 
 #define SDE_REGDMA_MODIFY(p, off, mask, data) \
 	do { \
-		SDEROT_DBG("SDEREG.M:[%s:0x%X] <= 0x%X\n", #off, (off),\
-				(u32)(data));\
-		writel_relaxed_no_log( \
-				(REGDMA_OP_REGMODIFY | \
-				 ((off) & REGDMA_ADDR_OFFSET_MASK)), \
-				p); \
-		p += sizeof(u32); \
-		writel_relaxed_no_log(mask, p); \
-		p += sizeof(u32); \
-		writel_relaxed_no_log(data, p); \
-		p += sizeof(u32); \
+		writel_relaxed(REGDMA_OP_REGMODIFY | \
+			((off) & REGDMA_ADDR_OFFSET_MASK), (p)); \
+		(p)++; \
+		writel_relaxed((mask), (p)); \
+		(p)++; \
+		writel_relaxed((data), (p)); \
+		(p)++; \
 	} while (0)
 
 #define SDE_REGDMA_BLKWRITE_INC(p, off, len) \
 	do { \
-		SDEROT_DBG("SDEREG.B:[%s:0x%X:0x%X]\n", #off, (off),\
-				(u32)(len));\
-		writel_relaxed_no_log( \
-				(REGDMA_OP_BLKWRITE_INC | \
-				 ((off) & REGDMA_ADDR_OFFSET_MASK)), \
-				p); \
-		p += sizeof(u32); \
-		writel_relaxed_no_log(len, p); \
-		p += sizeof(u32); \
+		writel_relaxed(REGDMA_OP_BLKWRITE_INC | \
+			((off) & REGDMA_ADDR_OFFSET_MASK), (p)); \
+		(p)++; \
+		writel_relaxed((len), (p)); \
+		(p)++; \
 	} while (0)
 
 #define SDE_REGDMA_BLKWRITE_DATA(p, data) \
 	do { \
-		SDEROT_DBG("SDEREG.I:[:] <= 0x%X\n", (u32)(data));\
-		writel_relaxed_no_log(data, p); \
-		p += sizeof(u32); \
-	} while (0)
-
-#define SDE_REGDMA_READ(p, data) \
-	do { \
-		data = readl_relaxed_no_log(p); \
-		p += sizeof(u32); \
+		writel_relaxed((data), (p)); \
+		(p)++; \
 	} while (0)
 
 /* Macro for directly accessing mapped registers */
