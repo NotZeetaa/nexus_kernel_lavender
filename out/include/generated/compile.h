49eabf28a98a92648393329ec2f424a5df0cbd80
Ryan Andri <ryanandri@linuxmail.org>
2018-01-02 02:44:39 +0700

    devfreq: add Simple GPU Algorithm.
    Authored: Paul Reioux aka Faux123 <reioux@gmail.com>
    
    This is an open source user configurable simple GPU Control Algorithm used to
    replace the closed sourced Qualcomm TrustZone GPU controller
    
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>
    Signed-off-by: Haseo97 <jawgattus@gmail.com>
    Signed-off-by: HachimanH20 <msuhartonoh20@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 3 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#2 SMP PREEMPT Sun Jan 24 10:53:11 WET 2021"
+#define UTS_VERSION "#3 SMP PREEMPT Sun Jan 24 11:35:38 WET 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
