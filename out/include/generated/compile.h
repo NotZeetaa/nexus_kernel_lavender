e042520667c4ce668cb98546ce09b64291951e5e
DarknessKiller <kaelvinchen2@gmail.com>
2019-04-09 14:10:40 +0800

    Compiled version
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>
    Signed-off-by: notzeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 11 */
+/* This file is auto generated, version 12 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#11 SMP PREEMPT Sun Nov 29 13:54:27 WET 2020"
+#define UTS_VERSION "#12 SMP PREEMPT Sun Nov 29 15:39:30 WET 2020"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
