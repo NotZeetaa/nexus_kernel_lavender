1df7ddfc222b23c714a2292a11290d716fdd4725
Ratoriku <a1063021545@gmail.com>
2020-10-11 10:41:25 +0800

    added some issues

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 7 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#4 SMP PREEMPT Fri Nov 27 19:06:20 WET 2020"
+#define UTS_VERSION "#7 SMP PREEMPT Sat Nov 28 01:58:45 WET 2020"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
