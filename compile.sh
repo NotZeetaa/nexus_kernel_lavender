28fbf9933f17ee9a9f905c44358c3b27df43c146
notzeetaa <rodrigo2005contente@gmail.com>
2020-11-29 14:04:08 +0000

    added clang 12
    Signed-off-by: notzeetaa <rodrigo2005contente@gmail.com>

--- compile.sh
+++ compile.sh
@@ -3,8 +3,8 @@ export KBUILD_BUILD_USER=Zeetaa
 export KBUILD_BUILD_HOST=ZeetaaPrjkt
 # Compile plox
 function compile() {
-    make -j$(nproc) O=out ARCH=arm64 lavender-perf_defconfig cc=" ccache clang-10"
-    make -j$(nproc) O=out ARCH=arm64  cc="ccache clang-10" \
+    make -j$(nproc) O=out ARCH=arm64 lavender-perf_defconfig cc=" ccache clang-12"
+    make -j$(nproc) O=out ARCH=arm64  cc="ccache clang-12" \
                                CROSS_COMPILE=aarch64-linux-gnu- \
                                CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 }
--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 10 */
+/* This file is auto generated, version 11 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#10 SMP PREEMPT Sat Nov 28 13:32:58 WET 2020"
+#define UTS_VERSION "#11 SMP PREEMPT Sun Nov 29 13:54:27 WET 2020"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
