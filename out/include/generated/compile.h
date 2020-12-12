b4a6ff8124fc13b938bb2216b0e54e94a4ad9484
NotZeetaa <rodrigo2005contente@gmail.com>
2020-12-12 18:30:19 +0000

    Update configs
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#1 SMP PREEMPT Sat Dec 12 17:36:55 WET 2020"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Dec 12 17:49:22 WET 2020"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
-#define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
+#define LINUX_COMPILER ""
