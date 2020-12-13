29507d13b68ca1bbb5306ab66cb64f16340f8724
NotZeetaa <rodrigo2005contente@gmail.com>
2020-12-13 09:27:50 +0000

    Disable GENTLE_FAIR_SLEEPERS
    improves responsiveness of ui
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 3 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#1 SMP PREEMPT Sat Dec 12 17:49:22 WET 2020"
+#define UTS_VERSION "#3 SMP PREEMPT Sat Dec 12 18:50:14 WET 2020"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
-#define LINUX_COMPILER ""
+#define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
