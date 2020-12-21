64024a180ffc3b96a19cd8ee2bc084a9ca8dcc2e
NotZeetaa <rodrigo2005contente@gmail.com>
2020-12-21 21:07:26 +0000

    New Build and Set 100hz config
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 5 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#5 SMP PREEMPT Sun Dec 13 10:05:46 WET 2020"
+#define UTS_VERSION "#2 SMP PREEMPT Mon Dec 21 14:20:09 WET 2020"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
--- out/include/generated/utsrelease.h
+++ out/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.4.248-NexusKernel-Lavender-Q-Old-V3"
+#define UTS_RELEASE "4.4.248-NexusKernel-Lavender-Q-Old-V4"
