b14a67ccb55b610d39216a828a25cd44a35e4951
NotZeetaa <rodrigo2005contente@gmail.com>
2020-12-23 07:40:04 +0000

    Edit DefConfig + New build
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 5 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#3 SMP PREEMPT Mon Dec 21 21:26:29 WET 2020"
+#define UTS_VERSION "#5 SMP PREEMPT Tue Dec 22 08:16:22 WET 2020"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
