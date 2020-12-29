8c808ca981ed207b22aa38d0d557d3c3499069f7
NotZeetaa <rodrigo2005contente@gmail.com>
2020-12-29 21:32:51 +0000

    Added Interactive X Governor
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -527,6 +527,9 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
+extern struct cpufreq_governor cpufreq_gov_interactivex;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactivex)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
