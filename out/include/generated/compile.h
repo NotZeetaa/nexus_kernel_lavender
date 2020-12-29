bf57ad19a658c0f02d7877963a9e6a3bff5f1ef8
NotZeetaa <rodrigo2005contente@gmail.com>
2020-12-29 21:07:55 +0000

    Add Interactive X Governor + New Build
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 7 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#4 SMP PREEMPT Sat Dec 26 10:59:20 WET 2020"
+#define UTS_VERSION "#7 SMP PREEMPT Tue Dec 29 20:50:17 WET 2020"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
