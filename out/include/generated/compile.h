f8d8cae596b4d8dd338d340d7d261d8a41f1d866
NotZeetaa <rodrigo2005contente@gmail.com>
2020-12-30 10:14:12 +0000

    Removed Tcp BBR
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 9 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#9 SMP PREEMPT Wed Dec 30 09:28:10 WET 2020"
+#define UTS_VERSION "#9 SMP PREEMPT Wed Dec 30 10:08:50 WET 2020"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
