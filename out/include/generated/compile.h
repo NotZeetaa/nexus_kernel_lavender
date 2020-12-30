f81abb011cf0e9d946e925612e45efcfa30e1e6a
NotZeetaa <rodrigo2005contente@gmail.com>
2020-12-30 17:16:00 +0000

    New Build & Compile.sh
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 10 */
+/* This file is auto generated, version 11 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#10 SMP PREEMPT Wed Dec 30 10:32:08 WET 2020"
+#define UTS_VERSION "#11 SMP PREEMPT Wed Dec 30 17:08:14 WET 2020"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
-#define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
+#define LINUX_COMPILER ""
