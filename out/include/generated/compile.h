33c575f2b997e2fa8f979ccb1fce9a2c9fa94695
NotZeetaa <rodrigo2005contente@gmail.com>
2020-12-30 09:50:20 +0000

    Added Tcp BBR & BBR2 in Makefile and Kconfig
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 8 */
+/* This file is auto generated, version 9 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#8 SMP PREEMPT Tue Dec 29 21:37:20 WET 2020"
+#define UTS_VERSION "#9 SMP PREEMPT Wed Dec 30 09:28:10 WET 2020"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
