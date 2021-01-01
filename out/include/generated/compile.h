0d16ff0681331526c41c4dfd2f83a4d4ea06b66d
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-31 20:29:30 -0800

    drm: Boost DDR bus upon running an atomic ioctl
    In order to reduce jank, request a DDR bus boost whenever an atomic
    ioctl is sent, which is what our diplay HAL uses to commit frames to the
    display. The boost should be sufficient enough to render the display's
    maximum FPS without any dropped frames when there is no significant
    external source of load.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 0 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#0 SMP PREEMPT Mon Jan 25 09:35:56 WET 2021"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Jan 25 10:00:11 WET 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
