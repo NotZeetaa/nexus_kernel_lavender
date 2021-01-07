8671050de72a9bd4f6424ee5c6d439b0978cf92a
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-06 21:00:31 -0800

    lot of CPU time is wasted on allocating, populati
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#1 SMP PREEMPT Sat Jan 23 17:06:25 WET 2021"
+#define UTS_VERSION "#2 SMP PREEMPT Sun Jan 24 10:53:11 WET 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
