ea08234d4a27eb078ef1de7a1c066f802f03201a
NotZeetaa <rodrigo2005contente@gmail.com>
2021-01-08 20:17:06 +0000

    Added Clang
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#1 SMP PREEMPT Fri Jan 8 19:25:34 WET 2021"
+#define UTS_VERSION "#2 SMP PREEMPT Fri Jan 8 20:09:13 WET 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
