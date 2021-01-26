40efd18068631306ce0fdba027d26508b69ce476
NotZeetaa <rodrigo2005contente@gmail.com>
2021-01-26 23:17:39 +0000

    OldCam
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#1 SMP PREEMPT Mon Jan 25 10:00:11 WET 2021"
+#define UTS_VERSION "#2 SMP PREEMPT Tue Jan 26 08:19:38 WET 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
--- out/include/generated/utsrelease.h
+++ out/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.4.253-NexusKernel-Lavender-Q-old-V7"
+#define UTS_RELEASE "4.4.253-NexusKernel-Lavender-Q-new-V7"
