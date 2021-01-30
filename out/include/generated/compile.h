45dcdac70472848dce637506f15877711e46b335
Ratoriku <a1063021545@gmail.com>
2021-01-30 23:02:51 +0800

    msm_thermal: Never offline cpus while throttling
    Signed-off-by: Ratoriku <a1063021545@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#4 SMP PREEMPT Sun Feb 14 09:23:50 WET 2021"
+#define LINUX_COMPILE_BY "Zeetaa"
+#define LINUX_COMPILE_HOST "ZeetaaPrjkt"
+#define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
---
+++ out/include/generated/vdso32-offsets.h
@@ -0,0 +1,4 @@
+#define vdso_offset_compat_rt_sigreturn_arm	0x0a60
+#define vdso_offset_compat_rt_sigreturn_thumb	0x0a80
+#define vdso_offset_compat_sigreturn_arm	0x0a50
+#define vdso_offset_compat_sigreturn_thumb	0x0a70
