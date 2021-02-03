a7c79d1c0280c4a78e52707b2228234a7633446f
NotZeetaa <rodrigo2005contente@gmail.com>
2021-02-03 23:27:09 +0000

    Fixed some errors + new build
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,11 +1,7 @@
-/* This file is auto generated, version 11 */
+/* This file is auto generated, version 12 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#11 SMP PREEMPT Sun Jan 31 17:43:41 WET 2021"
-/* This file is auto generated, version 7 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#7 SMP PREEMPT Sun Jan 31 14:07:41 WET 2021"
+#define UTS_VERSION "#12 SMP PREEMPT Wed Feb 3 21:35:49 WET 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
