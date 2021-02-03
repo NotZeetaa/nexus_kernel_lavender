a63d922dc70b93d65c156891a514cfd1cefb1de6
NotZeetaa <rodrigo2005contente@gmail.com>
2021-02-03 21:20:07 +0000

    Beta Build
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 10 */
+/* This file is auto generated, version 11 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#10 SMP PREEMPT Sun Jan 31 17:35:56 WET 2021"
+#define UTS_VERSION "#11 SMP PREEMPT Sun Jan 31 17:43:41 WET 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
