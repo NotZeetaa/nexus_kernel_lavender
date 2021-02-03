1e9eb77fb0da33cf3a94735ad0fcd86b6de7d368
NotZeetaa <rodrigo2005contente@gmail.com>
2021-02-03 21:21:45 +0000

    Revert "msm: mdss: Reduce latency while processing atomic ioctls"
    This reverts commit 9b6133c30838c33493df9c6dd314a7b3669bf364.
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -2,6 +2,10 @@
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
 #define UTS_VERSION "#11 SMP PREEMPT Sun Jan 31 17:43:41 WET 2021"
+/* This file is auto generated, version 7 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#7 SMP PREEMPT Sun Jan 31 14:07:41 WET 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
