a9f14273fbc7264e07d7b33fdaada07f5da54aa9
NotZeetaa <rodrigo2005contente@gmail.com>
2021-02-15 18:24:12 +0000

    defconfig: NewCam
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 8 */
+/* This file is auto generated, version 9 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#8 SMP PREEMPT Sun Feb 14 17:13:42 WET 2021"
+#define UTS_VERSION "#9 SMP PREEMPT Mon Feb 15 18:20:10 WET 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
-#define LINUX_COMPILER ""
+#define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
