7f371d317a528c83c5449a94768080834124f4e7
NotZeetaa <rodrigo2005contente@gmail.com>
2021-02-15 18:15:23 +0000

    panel: 66Hz
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 7 */
+/* This file is auto generated, version 8 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#7 SMP PREEMPT Sun Feb 14 10:55:02 WET 2021"
+#define UTS_VERSION "#8 SMP PREEMPT Sun Feb 14 17:13:42 WET 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
-#define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
+#define LINUX_COMPILER ""
--- out/include/generated/utsrelease.h
+++ out/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.4.257-NexusKernel-Lavender-Eas-V1-g4518b5cc8efe-dirty"
+#define UTS_RELEASE "4.4.257-NexusKernel-Lavender-Eas-V1-ga06c2874a924"
