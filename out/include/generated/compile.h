7e6b910d8e3030aa837815f55f23531d60cf88ec
NotZeetaa <rodrigo2005contente@gmail.com>
2021-02-15 18:58:29 +0000

    defconfig: OldCam
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 10 */
+/* This file is auto generated, version 11 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#10 SMP PREEMPT Mon Feb 15 18:30:52 WET 2021"
+#define UTS_VERSION "#11 SMP PREEMPT Mon Feb 15 18:38:23 WET 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
