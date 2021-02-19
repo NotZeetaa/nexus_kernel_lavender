1c3ee6faf4d5a3e0e9443b6c848ea8f2b9f6062d
Jason A. Donenfeld <Jason@zx2c4.com>
2021-02-19 15:08:33 +0100

    version: bump
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 /* This file is auto generated, version 4 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
 #define UTS_VERSION "#4 SMP PREEMPT Sat Mar 13 14:17:35 WET 2021"
+=======
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#3 SMP PREEMPT Fri Feb 26 08:00:51 WET 2021"
+>>>>>>> edfae1c3d853... version: bump
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
