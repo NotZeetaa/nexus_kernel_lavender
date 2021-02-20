93c6651576f7f29219f8e7c9340e5d4214673fe7
Sultan Alsawaf <sultan@kerneltoast.com>
2021-02-19 22:33:34 -0800

    sched/core: Free dead mm structs asynchronously in finish_task_switch()
    Although mm structs are not often freed from finish_task_switch() during
    a context switch, they can still slow things down and waste CPU time on
    high priority CPUs when freed. Since unbound workqueues are now affined
    to the little CPU cluster, we can offload the mm struct frees away from
    the current CPU entirely if it's a high-performance CPU, and defer them
    onto a little CPU. This reduces the amount of time spent in context
    switches and reclaims CPU time from more-important CPUs. This is
    achieved without increasing the size of the mm struct by reusing the
    mmput async work, which is guaranteed to not be in use by the time
    mm_count reaches zero.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#2 SMP PREEMPT Tue Feb 23 19:44:38 WET 2021"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Feb 25 19:39:02 WET 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER ""
