493c55cc6859aeb428a890751a48342d205220ce
NotZeetaa <rodrigo2005contente@gmail.com>
2021-02-25 19:47:18 +0000

    Revert "sched/core: Free dead mm structs asynchronously in finish_task_switch()"
    This reverts commit 93c6651576f7f29219f8e7c9340e5d4214673fe7.
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,11 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
+<<<<<<< HEAD
 #define UTS_VERSION "#1 SMP PREEMPT Thu Feb 25 19:44:12 WET 2021"
+=======
+#define UTS_VERSION "#2 SMP PREEMPT Tue Feb 23 19:44:38 WET 2021"
+>>>>>>> parent of 93c6651576f7... sched/core: Free dead mm structs asynchronously in finish_task_switch()
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
