3261069760ec393bca18eeb191022e61801c92fc
NotZeetaa <rodrigo2005contente@gmail.com>
2021-02-25 19:46:02 +0000

    out: Trash
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#1 SMP PREEMPT Thu Feb 25 19:39:02 WET 2021"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Feb 25 19:44:12 WET 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
-#define LINUX_COMPILER ""
+#define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
--- out/include/generated/utsrelease.h
+++ out/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.4.258-NexusKernel-Old-Lavender-Eas-V2-g17cfecbe4b11-dirty"
+#define UTS_RELEASE "4.4.258-NexusKernel-Old-Lavender-Eas-V2-g93c6651576f7"
