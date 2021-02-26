2579a1281401f9055f7838977448df329938fe34
NotZeetaa <rodrigo2005contente@gmail.com>
2021-02-26 13:01:20 +0000

    sched: Fix Some Warnings
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 6 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#4 SMP PREEMPT Fri Feb 26 08:40:18 WET 2021"
+#define UTS_VERSION "#6 SMP PREEMPT Fri Feb 26 12:42:34 WET 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
