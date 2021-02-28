eda014354f4577db7ebcdea36bc0533a655c0b95
NotZeetaa <rodrigo2005contente@gmail.com>
2021-02-28 09:29:14 +0000

    panel: 60Hz
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 12 */
+/* This file is auto generated, version 13 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#12 SMP PREEMPT Sun Feb 28 09:18:12 WET 2021"
+#define UTS_VERSION "#13 SMP PREEMPT Sun Feb 28 09:26:53 WET 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
--- out/include/generated/utsrelease.h
+++ out/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.4.258-NexusKernel-Old-Lavender-Eas-V2-g3d6b5ca2d102-dirty"
+#define UTS_RELEASE "4.4.258-NexusKernel-New-Lavender-Eas-V2-gad5df9a5e64d-dirty"
