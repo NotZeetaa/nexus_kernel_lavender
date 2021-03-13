d1094c18faca3c9606db506992b282268f8e36d1
NotZeetaa <rodrigo2005contente@gmail.com>
2021-03-13 14:14:06 +0000

    defconfig: Disabled CONFIG_THERMAL_MONITOR
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 3 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#2 SMP PREEMPT Sat Mar 13 12:25:45 WET 2021"
+#define UTS_VERSION "#3 SMP PREEMPT Sat Mar 13 14:05:28 WET 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
