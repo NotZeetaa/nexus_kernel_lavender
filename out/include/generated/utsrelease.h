9b92cf1e8abf3468eab534c43f0b8b34306a21e3
NotZeetaa <rodrigo2005contente@gmail.com>
2021-03-14 08:32:21 +0000

    panel: 66Hz
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/utsrelease.h
+++ out/include/generated/utsrelease.h
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-#define UTS_RELEASE "4.4.261-NexusKernel-Lavender-Q-old-V9"
-=======
-#define UTS_RELEASE "4.4.258-NexusKernel-Old-Lavender-Eas-V2-g36cdaf9a182d-dirty"
->>>>>>> edfae1c3d853... version: bump
+#define UTS_RELEASE "4.4.261-neXus-Lavender-old-V9"
