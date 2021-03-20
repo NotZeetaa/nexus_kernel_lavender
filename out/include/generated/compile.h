507559e8a2e37ccfe415b59f6edfd83e31900407
NotZeetaa <rodrigo2005contente@gmail.com>
2021-03-20 17:11:04 +0000

    out: Trash
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 7 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#7 SMP PREEMPT Thu Mar 18 20:59:16 WET 2021"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Mar 20 14:58:42 WET 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
