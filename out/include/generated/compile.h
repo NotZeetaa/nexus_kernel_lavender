10474f195493236d83e9779b65b9a5b4e8b05b88
NotZeetaa <rodrigo2005contente@gmail.com>
2021-03-20 17:44:57 +0000

    panel: 66Hz
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 3 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#2 SMP PREEMPT Sat Mar 20 17:30:53 WET 2021"
+#define UTS_VERSION "#3 SMP PREEMPT Sat Mar 20 17:43:30 WET 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
