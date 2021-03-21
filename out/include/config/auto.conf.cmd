9ed5a973d3daeeb2e648c8fc1ae627fa6a600057
Sultan Alsawaf <sultan@kerneltoast.com>
2021-03-20 21:39:17 -0700

    mm: Micro-optimize PID map reads for arm64
    Android and various applications in Android need to read PID map data in
    order to work. Some processes can contain over 10,000 mappings, which
    results in lots of time wasted on simply generating strings. This wasted
    time adds up, especially in the case of Unity-based games, which utilize
    the Boehm garbage collector. A game's main process typically has well
    over 10,000 mappings due to the loaded textures, and the Boehm GC reads
    PID maps several times a second. This results in over 100,000 map
    entries being printed out per second, so micro-optimization here is
    important. Before this commit, show_vma_header_prefix() would typically
    take around 1000 ns to run on a Snapdragon 855; now it only takes about
    50 ns to run, which is a 20x improvement.
    
    The primary micro-optimizations here assume that there are no more than
    40 bits in the virtual address space, hence the CONFIG_ARM64_VA_BITS
    check. Arm64 uses a virtual address size of 39 bits, so this perfectly
    covers it.
    
    This also removes padding used to beautify PID map output to further
    speed up reads and reduce the amount of bytes printed, and optimizes the
    dentry path retrieval for file-backed mappings.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/config/auto.conf.cmd
+++ out/include/config/auto.conf.cmd
@@ -1061,7 +1061,7 @@ deps_config := \
 include/config/auto.conf: \
 	$(deps_config)
 
-ifneq "$(KERNELVERSION)" "4.4.261"
+ifneq "$(KERNELVERSION)" "4.4.264"
 include/config/auto.conf: FORCE
 endif
 ifneq "$(ARCH)" "arm64"
--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#3 SMP PREEMPT Sat Mar 20 17:43:30 WET 2021"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Mar 30 14:43:19 WEST 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
