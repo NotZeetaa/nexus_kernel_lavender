3a852f301b322858e455df6eda22c0f3ad2c3878
NotZeetaa <rodrigo2005contente@gmail.com>
2021-04-02 16:00:55 +0100

    out: Trash
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-/* This file is auto generated, version 9 */
+/* This file is auto generated, version 10 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#9 SMP PREEMPT Fri Apr 2 15:41:00 WEST 2021"
-=======
-/* This file is auto generated, version 5 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#5 SMP PREEMPT Fri Apr 2 10:11:39 WEST 2021"
->>>>>>> parent of 5aed3c486bb9... defconfig: Disabled CONFIG_MSM_RPM_SMD
+#define UTS_VERSION "#10 SMP PREEMPT Fri Apr 2 15:56:53 WEST 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
