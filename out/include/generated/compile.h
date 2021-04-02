d3c2128ef6c57d2b043d6d499f47d18b9249d166
NotZeetaa <rodrigo2005contente@gmail.com>
2021-04-02 15:51:08 +0100

    Revert "defconfig: Disabled CONFIG_MSM_RPM_SMD"
    This reverts commit 5aed3c486bb91abd4cc0ae959692b096499760aa.
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 /* This file is auto generated, version 9 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
 #define UTS_VERSION "#9 SMP PREEMPT Fri Apr 2 15:41:00 WEST 2021"
+=======
+/* This file is auto generated, version 5 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#5 SMP PREEMPT Fri Apr 2 10:11:39 WEST 2021"
+>>>>>>> parent of 5aed3c486bb9... defconfig: Disabled CONFIG_MSM_RPM_SMD
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
