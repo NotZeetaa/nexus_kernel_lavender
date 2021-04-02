5aed3c486bb91abd4cc0ae959692b096499760aa
NotZeetaa <rodrigo2005contente@gmail.com>
2021-04-02 15:03:48 +0100

    defconfig: Disabled CONFIG_MSM_RPM_SMD
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 5 */
+/* This file is auto generated, version 6 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#5 SMP PREEMPT Fri Apr 2 10:11:39 WEST 2021"
+#define UTS_VERSION "#6 SMP PREEMPT Fri Apr 2 12:54:16 WEST 2021"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
