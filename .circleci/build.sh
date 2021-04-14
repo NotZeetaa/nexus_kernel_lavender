82c0b0c4a5ad6c57678e17dc6daa8cc442363aa0
NotZeetaa <rodrigo2005contente@gmail.com>
2021-04-14 21:55:18 +0100

    circleci: update
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -53,7 +53,7 @@ function compile() {
     make O=out ARCH=arm64 ${DEFCONFIG}
     make -j$(nproc --all) O=out \
                           ARCH=arm64 \
-			  CC="ccache clang" \
+			  CC=clang \
 			  CROSS_COMPILE=aarch64-linux-gnu- \
 			  CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 
